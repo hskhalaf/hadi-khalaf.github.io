e1953267d93293b4aed68f126e2c5470c2884e1b
Hadi Khalaf <119244138+hadi-khalaf@users.noreply.github.com>
2025-01-12 15:43:28 -0500

    new

--- src/data/news.ts
+++ src/data/news.ts
@@ -5,12 +5,4 @@ export interface News {
   link?: string;
 }
 
-export const newsData: News[] = [
-  // If you don't want to show news, just make the array empty.
-  {
-    date: "March 2024",
-    title: "Paper accepted at ICML 2024",
-    description: "Our work on causal discovery in time series data has been accepted at ICML 2024.",
-    link: "https://icml.cc/",
-  }
-];
+export const newsData: News[] = [];
